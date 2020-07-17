288f2c63ee9aa3f65212d5105e188b0b536ca217
wywang <727936199@qq.com>
2020-07-17 19:15:56 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,10 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
   mode: "development",
   entry: "./src/index.js",
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: "./dist",
+  },
   plugins: [
     new HtmlWebpackPlugin({
       title: "王月",
