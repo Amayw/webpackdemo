13c2b227459c22bcbb786e2be09368777ad22f51
wywang <727936199@qq.com>
2020-07-17 19:06:38 +0800

    添加css

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,24 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
   mode: "development",
   entry: "./src/index.js",
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "王月",
+      template: "src/assets/index.html",
+    }),
+  ],
   output: {
-    filename: "[name].[contenthash].js",
+    filename: "index.[contenthash].js",
     path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
   },
 };
